9d4aabb5d7317998de88489d8fea0fcacb11759b
Nan Sun <yogurt@ccs.neu.edu>
2014-11-19 17:13:45 -0500

    Add xml -> csv function with filter

---
+++ src/xml2csvTask1.java
@@ -0,0 +1,36 @@
+/*
+ * Usage: <xmlstyletask1.xslt> <input xml file> <output csv file>
+ */
+
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+import org.w3c.dom.Document;
+
+class xml2csvTask1 {
+
+    public static void main(String args[]) throws Exception {
+        File stylesheet = new File(args[0]);
+        File xmlSource = new File(args[1]);
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(xmlSource);
+
+        StreamSource stylesource = new StreamSource(stylesheet);
+        Transformer transformer = TransformerFactory.newInstance()
+                .newTransformer(stylesource);
+        Source source = new DOMSource(document);
+        Result outputTarget = new StreamResult(new File(args[2]));
+        transformer.transform(source, outputTarget);
+    }
+}
